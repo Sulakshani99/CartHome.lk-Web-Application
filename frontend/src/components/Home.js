55c7fd25e1b2087780b9622364f5a1b59f09673b
Bawantha Rathnayake <Bawantharathnayake395@gmail.com>
2024-06-29 08:56:03 +0530

    test

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -96,7 +96,7 @@ export default function Home() {
   return (
     <HomeContainer>
       <Logo>🚗</Logo>
-      <Title>Welcome to Your Vehicles</Title>
+      <Title>Welcome to Your Vehicles testing now 29</Title>
       <Description>
         Find 
       </Description>
